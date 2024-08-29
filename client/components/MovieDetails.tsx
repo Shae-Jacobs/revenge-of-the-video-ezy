1657e614de9186b06fc01aef6643090d7e8acef3
Jeremy Webster <jeremy.webster106@gmail.com>
2024-08-30 11:16:09 +1200

    add movie details content

--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -4,24 +4,23 @@ import { useMovieById } from "../hooks/useMovieById"
 function MovieDetails() {
 
   const { id } = useParams()
-  const { data, isPending, isError } = useMovieById(String(id))
+  const movie = useMovieById(String(id))
 
-  if(isPending){
+  if(movie.isPending){
     return <p>Loading...</p>
   }
 
-  if(isError){
+  if(movie.isError){
     return <p>An error occurred loading movies...</p>
   }
 
   return (
     <>
-    <h2>Movie Details</h2>
     <div className='two-columns'>
       <div>
-        <h2>Title: movie title (1930)</h2>
-        <p>Genre: sadfka</p>
-        <img src="" alt="Poster for XXX"/>
+        <h2>{`${movie.data.title} (${movie.data.year})`}</h2>
+        <p>{`${movie.data.genre}`}</p>
+        <img src="" alt={`Poster for ${movie.data.title}`} className="movie-poster"/>
       </div>
       <div>
         <h2>Review 1</h2>
