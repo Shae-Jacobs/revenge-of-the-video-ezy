1c2206819c415b834118cac3f099c5ec84d09555
Jeremy Webster <jeremy.webster106@gmail.com>
2024-08-30 10:46:25 +1200

    progress

--- client/apis/movies.ts
+++ client/apis/movies.ts
@@ -1,6 +1,13 @@
 import request from "superagent";
+import { Movie } from "../../models/movies";
 
 export async function getAllMovies(){
   const res = await request.get('/api/v1/movies')  
-  return res.body
+  return res.body as Movie[]
 }
+
+export async function getMoviesById(id:string){
+  const res = await request.get(`/api/v1/movies/${id}`)  
+  return res.body as Movie
+}
+
--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -1,8 +1,37 @@
+import { useParams } from "react-router-dom"
+import { useMovieById } from "../hooks/useMovieById"
+
 function MovieDetails() {
+
+  const { id } = useParams()
+  const { data, isPending, isError } = useMovieById(String(id))
+
+  if(isPending){
+    return <p>Loading...</p>
+  }
+
+  if(isError){
+    return <p>An error occurred loading movies...</p>
+  }
 
   return (
     <>
     <h2>Movie Details</h2>
+    <div className='two-columns'>
+      <div>
+        <h2>Title: movie title (1930)</h2>
+        <p>Genre: sadfka</p>
+        <img src="" alt="Poster for XXX"/>
+      </div>
+      <div>
+        <h2>Review 1</h2>
+        <p>Big old review text.Big old review textBig old review textBig old review textBig old review textBig old review textBig old review textBig old review text</p>
+        <h2>Review 2</h2>
+        <p>Big old review text.Big old review textBig old review textBig old review textBig old review textBig old review textBig old review textBig old review text</p>
+        <h2>Review 3</h2>
+        <p>Big old review text.Big old review textBig old review textBig old review textBig old review textBig old review textBig old review textBig old review text</p>
+      </div>
+    </div>
     </>
   )
 }
