4b7738cbd29b97598f31cc4eda2dbd19a908209e
darcy <darcy.abey1@gmail.com>
2024-08-30 10:28:15 +1200

    migrations and routes

---
+++ server/db/index.ts
@@ -0,0 +1,78 @@
+import connection from './connection.ts'
+
+// Movies functions
+export const getAllMovies = async () => {
+  return connection('movies').select()
+}
+
+export const getMovieById = async (id: number) => {
+  return connection('movies').where({ id }).first()
+}
+
+export const createMovie = async (
+  title: string,
+  year: number,
+  genre: string,
+) => {
+  const [id] = await connection('movies').insert({ title, year, genre })
+  return { id, title, year, genre }
+}
+
+export const updateMovie = async (
+  id: number,
+  updates: { title?: string; year?: number; genre?: string },
+) => {
+  // Perform the update and return the updated record
+  await connection('movies').where({ id }).update(updates)
+  return { id, ...updates }
+}
+
+export const deleteMovie = async (id: number) => {
+  // Perform the delete and return a success message
+  await connection('movies').where({ id }).del()
+  return { id }
+}
+
+// Reviews functions
+export const getAllReviews = async () => {
+  return connection('reviews').select('*')
+}
+
+export const getReviewById = async (id: number) => {
+  return connection('reviews').where({ id }).first()
+}
+
+export const createReview = async (
+  movie_id: number,
+  review_text: string,
+  movie_rating: number,
+  reviewer_name: string,
+) => {
+  const [id] = await connection('reviews').insert({
+    movie_id,
+    review_text,
+    movie_rating,
+    reviewer_name,
+  })
+  return { id, movie_id, review_text, movie_rating, reviewer_name }
+}
+
+export const updateReview = async (
+  id: number,
+  updates: {
+    movie_id?: number
+    review_text?: string
+    movie_rating?: number
+    reviewer_name?: string
+  },
+) => {
+  // Perform the update and return the updated record
+  await connection('reviews').where({ id }).update(updates)
+  return { id, ...updates }
+}
+
+export const deleteReview = async (id: number) => {
+  // Perform the delete and return a success message
+  await connection('reviews').where({ id }).del()
+  return { id }
+}
---
+++ server/db/migrations/20240829185735_movies.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('movies', (table) => {
+    table.increments('id')
+    table.string('title')
+    table.integer('year')
+    table.string('genre')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('movies')
+}
